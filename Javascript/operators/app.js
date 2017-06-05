5fa6b6e8772ba765dd2fc96210445bac818297f2
Dan McGuinness <dan@gemdatacorp.com>
2017-06-05 08:48:24 -0400

    adds js operatorsand async callbacks

---
+++ Javascript/operators/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Dan1 on 6/5/17.
+ */
+
+// var a = 3 + 4;
+// var a = 3 < 4;
+// var a = 3 + 4 * 5;
+
+var a = 3, b = 3, c = 4;
+a = b = c;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+document.write("yo!");
